27ee42d8e03283e723f1c6e5a2b63776eb0f3c81
Michael Sawyers <5084015+sawyers@users.noreply.github.com>
2020-07-09 16:27:34 -0700

    Need to go to debugger but seems like a good pattern once it works

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+#!/usr/bin/env python3
+"""
+Learning project to have better standards of writing python
+"""
+
 import random
 import textwrap
 from tinydb import TinyDB, Query
@@ -6,48 +11,52 @@ wrapper = textwrap.TextWrapper(width=60)
 db = TinyDB("./legends.json")
 
 
-class char:
-    def __init__(self):
-        self.data = []
+genders = ["Female", "Male"]
 
 
-def roll(roll, modify=0):
-    (to_roll, size) = [int(s) for s in roll.split("d")]
-
+def roll(to_roll, high, modifier=0):
+    """ because adding #nosec everwhere would be a drag"""
     i = 1
     total = 0
     while i <= to_roll:
-        total += random.randint(1, size)  # nosec
+        total += random.randint(1, high)  # nosec
         i += 1
-    total += modify
-    if total < 0:
-        total = 0
+    total += modifier
     return total
 
 
-def base_stats():
-    work_stats = {}
-    stats_lst = ["str", "dex", "int", "con", "cha", "wis"]
-    for i in stats_lst:
-        work_stats[i] = roll("3d6")
-    return work_stats
-
-
-def char_race(roll, modify=0):
+def race_search():
+    """lookup against table 101 with optional table 101b"""
+    race_roll = roll(1, 20)
+    my_race = ""
+    my_desc = ""
     table = db.table("101")
     result = Query()
-    result = table.search((result.log <= roll) & (race.high >= roll))[0]
+    result = table.search((result.low <= race_roll) & (result.high >= race_roll))
+    print(result)
+    return my_race, my_desc
 
 
-my_char = char()
-my_char.stats = base_stats()
-my_char.race, my_char.race_desc = char_race(roll("1d20"))
-# my_char.culture, my_char.cmod = cmod()
+print("Gender: %s\n" % genders[roll(1, len(genders), -1)])
+race, desc = race_search()
 
-# print("Race: %s\n" % my_char.race)
-# print("Race Descriptions: \n\n\t%s\n" % wrapper.fill(text=my_char.race_desc))
-print("Stats:")
-for i in my_char.stats:
-    tmp_stat_name = i.capitalize()
-    tmp_stat_value = my_char.stats[i]
-    print("\t{0} : {1}".format(tmp_stat_name, tmp_stat_value))
+
+# def base_stats():
+
+#     work_stats = {}
+#     stats_lst = ["str", "dex", "int", "con", "cha", "wis"]
+#     for i in stats_lst:
+#         work_stats[i] = roll("3d6")
+#     return work_stats
+
+
+# my_char.race, my_char.race_desc = char_race(roll("1d20"))
+# # my_char.culture, my_char.cmod = cmod()
+
+# # print("Race: %s\n" % my_char.race)
+# # print("Race Descriptions: \n\n\t%s\n" % wrapper.fill(text=my_char.race_desc))
+# print("Stats:")
+# for i in my_char.stats:
+#     tmp_stat_name = i.capitalize()
+#     tmp_stat_value = my_char.stats[i]
+#     print("\t{0} : {1}".format(tmp_stat_name, tmp_stat_value))

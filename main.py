9cb240889cdd098d4d4b7f30da45ddf35fa6ca9a
Michael <5084015+sawyers@users.noreply.github.com>
2023-03-22 15:16:58 +0000

    Able to make it into an actual class object and started adding data.
    Making more elements will mean adding to the json db so not super quick and
    mostly is just a case of adding more functions to the class.
    
    Would also like to format it to a sheet at the end, maybe with a jinja template
    
    Another nice one would be to break the class into a module to practice that.

--- main.py
+++ main.py
@@ -8,55 +8,101 @@ import textwrap
 from tinydb import TinyDB, Query
 
 wrapper = textwrap.TextWrapper(width=60)
-db = TinyDB("./legends.json")
 
 
-genders = ["Female", "Male"]
+class generator:
+    # db = TinyDB("./legends.json")
+
+    def __init__(self):
+        genders = ["Female", "Male"]
+        self.gender = genders[random.randint(0, 1)]
+      
+        self.db = TinyDB("./legends.json")
+
+    def add_stat(self, stat_name):
+        setattr(self, stat_name, random.randint(3, 18))
+
+    def gender(self):
+        genders = ["Female", "Male"]
+        # self.gender = genders[random.randint(0, 1)]
+        self.gender = self.add_stat('test')
+
+    def race(self):
+        race_roll = random.randint(1, 20)
+
+        if race_roll == 20:
+            table = self.db.table("101a")
+            race_roll = random.randint(1, 10)
+        else:
+            table = self.db.table("101")
+
+        result = Query()
+        result = table.search((result.low <= race_roll)
+                              & (result.high >= race_roll))[0]
+
+        detented_text = textwrap.dedent(result["desc"].strip())
+
+        self.race = result["race"]
+        self.race_desc = textwrap.fill(detented_text, width=70)
+
+    def culture(self):
+        cu_roll = random.randint(1, 10)
+        table = self.db.table("102")
+        result = Query()
+        result = table.search((result.low <= cu_roll)
+                              & (result.high >= cu_roll))[0]
+
+        detented_text = textwrap.dedent(result["desc"].strip())
+        desc = textwrap.fill(detented_text, width=70)
+
+        self.culture = result["culture"]
+        self.culture_desc = desc
+        self.culture_mod = result["cumod"]
+
+    def social(self):
+        soc_roll = random.randint(1, 100) + int(self.culture_mod)
+        table = self.db.table("103")
+        result = Query()
+        result = table.search((result.low <= soc_roll)
+                              & (result.high >= soc_roll))[0]
+
+        detented_text = textwrap.dedent(result["desc"].strip())
+        desc = textwrap.fill(detented_text, width=70)
+
+        self.social = result["social"],
+        self.social_desc = desc
+        self.social_mod = result["somod"]
+
+    def trait(self):
+        trait_roll = random.randint(1, 100)
+
+        if trait_roll <= 50:
+            self.trait = "No special personality trait"
+
+        if 51 <= trait_roll <= 65:
+            table = self.db.table("318B")
+        elif 66 <= trait_roll <= 80:
+            table = self.db.table("647")
+        else:
+            table = self.db.table("648")
+
+        spec_trait = random.randint(2, 20)
+
+        result = Query()
+        result = table.search((result.roll == spec_trait))[0]
+        self.trait = result["result"]
 
 
-def roll(to_roll, high, modifier=0):
-    """ because adding #nosec everwhere would be a drag"""
-    i = 1
-    total = 0
-    while i <= to_roll:
-        total += random.randint(1, high)  # nosec
-        i += 1
-    total += modifier
-    return total
+char = generator()
 
+stats_lst = ["Str", "Dex", "Int", "Con", "Cha", "Wis"]
 
-def race_search():
-    """lookup against table 101 with optional table 101b"""
-    race_roll = roll(1, 20)
-    my_race = ""
-    my_desc = ""
-    table = db.table("101")
-    result = Query()
-    result = table.search((result.low <= race_roll) & (result.high >= race_roll))
-    print(result)
-    return my_race, my_desc
+for stat in stats_lst:
+    char.add_stat(stat)
 
+char.race()
+char.culture()
+char.social()
+char.trait()
 
-print("Gender: %s\n" % genders[roll(1, len(genders), -1)])
-race, desc = race_search()
-
-
-# def base_stats():
-
-#     work_stats = {}
-#     stats_lst = ["str", "dex", "int", "con", "cha", "wis"]
-#     for i in stats_lst:
-#         work_stats[i] = roll("3d6")
-#     return work_stats
-
-
-# my_char.race, my_char.race_desc = char_race(roll("1d20"))
-# # my_char.culture, my_char.cmod = cmod()
-
-# # print("Race: %s\n" % my_char.race)
-# # print("Race Descriptions: \n\n\t%s\n" % wrapper.fill(text=my_char.race_desc))
-# print("Stats:")
-# for i in my_char.stats:
-#     tmp_stat_name = i.capitalize()
-#     tmp_stat_value = my_char.stats[i]
-#     print("\t{0} : {1}".format(tmp_stat_name, tmp_stat_value))
+print(char.__dict__)

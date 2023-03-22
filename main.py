984818ea8687fd68b51de9b0d7ca404a0f184d40
Michael <5084015+sawyers@users.noreply.github.com>
2023-03-22 08:26:01 -0700

    Update main.py

--- main.py
+++ main.py
@@ -2,165 +2,106 @@
 """
 Learning project to have better standards of writing python
 """
-
 import random
 import textwrap
-import logging
 from tinydb import TinyDB, Query
 
-logging.basicConfig(format="%(levelname)s:%(message)s", level=logging.DEBUG)
-
-wrapper = textwrap.TextWrapper()
-db = TinyDB("data/legends.json")
-genders = ["Female", "Male"]
-stats_lst = ["str", "dex", "int", "con", "cha", "wis"]
+wrapper = textwrap.TextWrapper(width=60)
 
 
-def roll(to_roll, high, modifier=0):
-    """ because adding #nosec everwhere would be a drag"""
-    i = 1
-    total = 0
-    while i <= to_roll:
-        total += random.randint(1, high)  # nosec
-        i += 1
-    total += modifier
-    return total
-
-
-def race_search():
-    """lookup against table 101 with optional table 101b"""
-    race_roll = roll(1, 20)
-    my_race = ""
-    my_desc = ""
-
-    if race_roll == 20:
-        table = db.table("101a")
-        race_roll = roll(1, 10)
-    else:
-        table = db.table("101")
-
-    result = Query()
-    result = table.search((result.low <= race_roll) & (result.high >= race_roll))[0]
-
-    detented_text = textwrap.dedent(result["desc"].strip())
-    desc = textwrap.fill(detented_text, width=70)
-    return result["race"], desc
-
-
-def base_culture():
-    """Determine base culture"""
-    cu_roll = roll(1, 10)
-    table = db.table("102")
-    result = Query()
-    result = table.search((result.low <= cu_roll) & (result.high >= cu_roll))[0]
-
-    detented_text = textwrap.dedent(result["desc"].strip())
-    desc = textwrap.fill(detented_text, width=70)
-
-    return result["culture"], desc, result["cumod"]
-
-
-def base_social(cumod):
-    """Determine social standing, impacted by culture level"""
-    try:
-        soc_roll = roll(1, 100) + int(cumod)
-        table = db.table("103")
-        result = Query()
-        result = table.search((result.low <= soc_roll) & (result.high >= soc_roll))[0]
-    except ValueError:
-        logging.error("Somehow the cumod is not an integer")
-        raise
-    detented_text = textwrap.dedent(result["desc"].strip())
-    desc = textwrap.fill(detented_text, width=70)
-
-    return result["social"], desc, result["somod"]
-
-
-def personal_trait():
-    """Basic personality"""
-    trait_roll = roll(1, 100)
-    if trait_roll <= 50:
-        return "No special personality trait"
-    elif 51 <= trait_roll <= 65:
-        table = db.table("318B")
-    elif 66 <= trait_roll <= 80:
-        table = db.table("647")
-    else:
-        table = db.table("648")
-
-    spec_trait = roll(2, 20)
-
-    result = Query()
-    result = table.search((result.roll == spec_trait))[0]
-    return result["result"]
-
-
-class NpcCard(object):
-    gender = ""
-    race = ""
-    race_desc = ""
-    attributes = {}
+class generator:
+    # db = TinyDB("./data/legends.json")
 
     def __init__(self):
-        self.gender = genders[roll(1, len(genders), -1)]
-        for i in stats_lst:
-            self.attributes[i] = roll(3, 6)
-        self.race, self.race_desc = race_search()
-        self.trait = personal_trait()
-        self.culture, self.cu_desc, self.cumod = base_culture()
-        self.socmod, self.socdesc, self.somod = base_social(self.cumod)
+        genders = ["Female", "Male"]
+        self.gender = genders[random.randint(0, 1)]
+      
+        self.db = TinyDB("./legends.json")
 
-    def print(self):
-        card = """\
-Race: {}
+    def add_stat(self, stat_name):
+        setattr(self, stat_name, random.randint(3, 18))
 
-Racial description:
+    def gender(self):
+        genders = ["Female", "Male"]
+        # self.gender = genders[random.randint(0, 1)]
+        self.gender = self.add_stat('test')
 
-{}
+    def race(self):
+        race_roll = random.randint(1, 20)
 
-Gender: {}
+        if race_roll == 20:
+            table = self.db.table("101a")
+            race_roll = random.randint(1, 10)
+        else:
+            table = self.db.table("101")
 
-    Str: {}
-    Dex: {}
-    Con: {}
-    Int: {}
-    Wis: {}
-    Cha: {}
+        result = Query()
+        result = table.search((result.low <= race_roll)
+                              & (result.high >= race_roll))[0]
 
-Background:
+        detented_text = textwrap.dedent(result["desc"].strip())
 
-Personality: {}
+        self.race = result["race"]
+        self.race_desc = textwrap.fill(detented_text, width=70)
 
-Base Culture: {}
-Base Culture description:
+    def culture(self):
+        cu_roll = random.randint(1, 10)
+        table = self.db.table("102")
+        result = Query()
+        result = table.search((result.low <= cu_roll)
+                              & (result.high >= cu_roll))[0]
 
-{}
+        detented_text = textwrap.dedent(result["desc"].strip())
+        desc = textwrap.fill(detented_text, width=70)
 
-Social Standing: {}
-Social Description:
+        self.culture = result["culture"]
+        self.culture_desc = desc
+        self.culture_mod = result["cumod"]
 
-{}
-        """.format(
-            self.race,
-            self.race_desc,
-            self.gender,
-            self.attributes["str"],
-            self.attributes["dex"],
-            self.attributes["con"],
-            self.attributes["int"],
-            self.attributes["wis"],
-            self.attributes["cha"],
-            self.trait,
-            self.culture,
-            self.cu_desc,
-            self.socmod,
-            self.socdesc,
-        )
+    def social(self):
+        soc_roll = random.randint(1, 100) + int(self.culture_mod)
+        table = self.db.table("103")
+        result = Query()
+        result = table.search((result.low <= soc_roll)
+                              & (result.high >= soc_roll))[0]
 
-        print(textwrap.dedent(card))
+        detented_text = textwrap.dedent(result["desc"].strip())
+        desc = textwrap.fill(detented_text, width=70)
+
+        self.social = result["social"],
+        self.social_desc = desc
+        self.social_mod = result["somod"]
+
+    def trait(self):
+        trait_roll = random.randint(1, 100)
+
+        if trait_roll <= 50:
+            self.trait = "No special personality trait"
+
+        if 51 <= trait_roll <= 65:
+            table = self.db.table("318B")
+        elif 66 <= trait_roll <= 80:
+            table = self.db.table("647")
+        else:
+            table = self.db.table("648")
+
+        spec_trait = random.randint(2, 20)
+
+        result = Query()
+        result = table.search((result.roll == spec_trait))[0]
+        self.trait = result["result"]
 
 
-if __name__ == "__main__":
-    logging.info("Starting NPC maker")
-    NPC = NpcCard()
-    NPC.print()
+char = generator()
+
+stats_lst = ["Str", "Dex", "Int", "Con", "Cha", "Wis"]
+
+for stat in stats_lst:
+    char.add_stat(stat)
+
+char.race()
+char.culture()
+char.social()
+char.trait()
+
+print(char.__dict__)

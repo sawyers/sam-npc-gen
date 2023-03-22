302e0371b92ffd9d8a74bc35b33077e18db6f267
repl.it user <replituser@example.com>
2023-03-22 15:21:58 +0000

    conflicts?

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ import logging
 from tinydb import TinyDB, Query
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 wrapper = textwrap.TextWrapper(width=60)
 
 
@@ -17,10 +18,15 @@ class generator:
 =======
 logging.basicConfig(format="%(levelname)s:%(message)s", level=logging.DEBUG)
 
+=======
+logging.basicConfig(format="%(levelname)s:%(message)s", level=logging.DEBUG)
+
+>>>>>>> origin/main
 wrapper = textwrap.TextWrapper()
 db = TinyDB("data/legends.json")
 genders = ["Female", "Male"]
 stats_lst = ["str", "dex", "int", "con", "cha", "wis"]
+<<<<<<< HEAD
 >>>>>>> 9bb8c4a6fb0fe88e4b9ea5efe76ecbd69bfca972
 
     def __init__(self):
@@ -69,6 +75,8 @@ stats_lst = ["str", "dex", "int", "con", "cha", "wis"]
         self.culture = result["culture"]
         self.culture_desc = desc
         self.culture_mod = result["cumod"]
+=======
+>>>>>>> origin/main
 
     def social(self):
         soc_roll = random.randint(1, 100) + int(self.culture_mod)
@@ -132,6 +140,7 @@ def race_search():
 
     result = Query()
     result = table.search((result.low <= race_roll) & (result.high >= race_roll))[0]
+<<<<<<< HEAD
 
     detented_text = textwrap.dedent(result["desc"].strip())
     desc = textwrap.fill(detented_text, width=70)
@@ -251,8 +260,132 @@ Social Description:
         print(textwrap.dedent(card))
 
 
+=======
+
+    detented_text = textwrap.dedent(result["desc"].strip())
+    desc = textwrap.fill(detented_text, width=70)
+    return result["race"], desc
+
+
+def base_culture():
+    """Determine base culture"""
+    cu_roll = roll(1, 10)
+    table = db.table("102")
+    result = Query()
+    result = table.search((result.low <= cu_roll) & (result.high >= cu_roll))[0]
+
+    detented_text = textwrap.dedent(result["desc"].strip())
+    desc = textwrap.fill(detented_text, width=70)
+
+    return result["culture"], desc, result["cumod"]
+
+
+def base_social(cumod):
+    """Determine social standing, impacted by culture level"""
+    try:
+        soc_roll = roll(1, 100) + int(cumod)
+        table = db.table("103")
+        result = Query()
+        result = table.search((result.low <= soc_roll) & (result.high >= soc_roll))[0]
+    except ValueError:
+        logging.error("Somehow the cumod is not an integer")
+        raise
+    detented_text = textwrap.dedent(result["desc"].strip())
+    desc = textwrap.fill(detented_text, width=70)
+
+    return result["social"], desc, result["somod"]
+
+
+def personal_trait():
+    """Basic personality"""
+    trait_roll = roll(1, 100)
+    if trait_roll <= 50:
+        return "No special personality trait"
+    elif 51 <= trait_roll <= 65:
+        table = db.table("318B")
+    elif 66 <= trait_roll <= 80:
+        table = db.table("647")
+    else:
+        table = db.table("648")
+
+    spec_trait = roll(2, 20)
+
+    result = Query()
+    result = table.search((result.roll == spec_trait))[0]
+    return result["result"]
+
+
+class NpcCard(object):
+    gender = ""
+    race = ""
+    race_desc = ""
+    attributes = {}
+
+    def __init__(self):
+        self.gender = genders[roll(1, len(genders), -1)]
+        for i in stats_lst:
+            self.attributes[i] = roll(3, 6)
+        self.race, self.race_desc = race_search()
+        self.trait = personal_trait()
+        self.culture, self.cu_desc, self.cumod = base_culture()
+        self.socmod, self.socdesc, self.somod = base_social(self.cumod)
+
+    def print(self):
+        card = """\
+Race: {}
+
+Racial description:
+
+{}
+
+Gender: {}
+
+    Str: {}
+    Dex: {}
+    Con: {}
+    Int: {}
+    Wis: {}
+    Cha: {}
+
+Background:
+
+Personality: {}
+
+Base Culture: {}
+Base Culture description:
+
+{}
+
+Social Standing: {}
+Social Description:
+
+{}
+        """.format(
+            self.race,
+            self.race_desc,
+            self.gender,
+            self.attributes["str"],
+            self.attributes["dex"],
+            self.attributes["con"],
+            self.attributes["int"],
+            self.attributes["wis"],
+            self.attributes["cha"],
+            self.trait,
+            self.culture,
+            self.cu_desc,
+            self.socmod,
+            self.socdesc,
+        )
+
+        print(textwrap.dedent(card))
+
+
+>>>>>>> origin/main
 if __name__ == "__main__":
     logging.info("Starting NPC maker")
     NPC = NpcCard()
     NPC.print()
+<<<<<<< HEAD
 >>>>>>> 9bb8c4a6fb0fe88e4b9ea5efe76ecbd69bfca972
+=======
+>>>>>>> origin/main
